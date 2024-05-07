19b79cf346eba815e3e57b91b369e17949fcbd7d
HYEONMIN <135511648+Hyeonmin072@users.noreply.github.com>
2024-05-07 18:28:18 +0900

    [Bronze V] Title: 연세대학교, Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        if(a==0)
+            System.out.println("YONSEI");
+        if(a==1)
+            System.out.println("Leading the Way to the Future");
+    }
+}
