9402f3429ecb8ad4afbacc6f7e82e88b17ca66fd
HYEONMIN <135511648+Hyeonmin072@users.noreply.github.com>
2024-08-29 18:19:08 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(int num) {
+        
+        String answer ="";
+        if(num==0){
+            answer = "Even";
+        }
+        
+        else if(num%2==0){
+            answer = "Even";
+        }
+        
+        else{
+            answer = "Odd";
+        }
+        
+        
+        return answer;
+    }
+}
